f657222fbe9518cd39131f419fffee1dcc474f16
DaveyClan <116041150+DaveyClan@users.noreply.github.com>
2026-04-16 12:44:21 +1000

    Create startup.py

---
+++ matrix/_zip/plugin.program.daveyclanwizard/startup.py
@@ -0,0 +1,8 @@
+# Startup code for the DaveyClan Wizard Plugin
+import xbmc
+
+def startup():
+    xbmc.log("DaveyClan Wizard startup initiated", xbmc.LOGINFO)
+
+if __name__ == '__main__':
+    startup()
